5c153c8931ab9d8d221dbfcdf4b2b34d6ab912c6
nquinbla <nquinbla@myuax.com>
2023-11-06 20:51:40 +0100

    (1) Vídeo
    Primer programa en C++

---
+++ (1) Primer programa/main1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Nerea del futuro!" << std::endl;
+    return 0;
+}
---
+++ (1) Primer programa/main2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "Hola Nerea del futuro!" << endl;
+    return 0;
+}
